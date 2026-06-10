cf951ae80e5e8ed3f9cb95e7d79b0e4844fe186b
Jorge Matos <jisapjisap@gmail.com>
2026-06-10 18:45:56 +0200

    feat: create Index page with hero section, service cards, and agency overview components

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -696,11 +696,52 @@ const Index = () => {
               News & Blog
             </span>
 
-            <h2 className="text-2xl sm:text-6xl font-semibold sm:max-w-3xl mt-5 leading-tight text-white">
+            <h2 className="text-4xl md:text-3xl lg:text-6xl font-semibold sm:max-w-5xl mt-5 leading-tight text-white">
               Insights, Thoughts, Industry Trends, Marketing Tips.
             </h2>
           </div>
         </div>
+
+        <div className="blog-wrapper grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10">
+          {blogs.map((blog) => (
+            <div
+              key={blog.id}
+              className="blog-item group flex flex-col text-white"
+            >
+              <div className="blog-item group flex flex-col text-white">
+                <img
+                  src={blog.image}
+                  alt={blog.title}
+                  className="group-hover:scale-110 transition-all duration-300"
+
+                />
+              </div>
+
+              <div className="blog-content pt-5">
+                <p className="text-lg font-semibold pb-2">
+                  <span className="bg-lime-300 text-black px-3 py-1 rounded-full me-3">
+                    {blog.date}
+                  </span>
+
+                  {blog.category}
+                </p>
+
+                <Link
+                  to={`/blog/${blog.id}`}
+                  className="text-xl font-semibold hover:text-lime-300 transition-all duration-300"
+                >
+                  {blog.title}
+                </Link>
+
+                <p className="text-md text-gray-300 mt-3">
+                  {blog.description}
+                </p>
+              </div>
+            </div>
+
+          ))}
+        </div>
+
 
 
       </div>
