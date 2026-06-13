9a2f538cc8fca5b2e26ce1a0f523a528d7350719
Jorge Matos <jisapjisap@gmail.com>
2026-06-13 11:17:03 +0200

    feat: initialize project structure with global styles and React routing for About page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Nav from './Components/Nav'
 import Index from './Pages/Index'
 import Footer from './Components/Footer'
+import About from './Pages/About'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
           <Nav />
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/about" element={<About />} />
           </Routes>
           <Footer />
         </BrowserRouter>
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,6 +1,123 @@
+import { Link } from "react-router-dom"
+import title_icon from "../assets/title_icon.svg"
+import { Icon } from "@iconify/react"
+import aboutLogo from "../assets/novo-about-logo.png"
+import CountUp from "../Components/CountUp"
+
+
+
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <div className="section-banner h-[500px] bg-[#121212] px-[2%] md:px-[8%] xl:px-[12%] flex flex-col justify-end items-start text-white w-full">
+        <h2 className="text-5xl lg:text-7xl font-semibold w-full lg:max-w-2xl relative pb-3">
+          About Us
+          <span className="absolute hidden sm:flex top-0 right-35 w-10 h-10">
+            <img
+              src={title_icon}
+              alt="icon"
+              className="w-full h-full"
+            />
+          </span>
+        </h2>
+
+        <p className="border-b-2 w-full pb-10 border-gray-300">
+          Creative studio at the intersection of art, designand technology.
+        </p>
+
+        <ul className="flex gap-8 py-5">
+          <li>
+            <Link to="/index" className="font-normal text-white text-lg">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="font-normal text-white text-lg">
+              About us
+            </Link>
+          </li>
+        </ul>
+
+      </div>
+
+      <div className="about py-[20%] xl:py-[8%] px-[2%] md:px-[8%] xl:px-[12%] flex justify-between items-start lg:flex-row flex-col gap-10">
+        <div className="about-content w-full lg:w-[60%]">
+          <span className="text-black bg-primary px-2 py-3 font-semibold text-md sm:text-xl rounded-sm">
+            Welcome to Nova
+          </span>
+
+          <h2 className="text-3xl sm:text-6xl font-semibold sm:max-w-3xl my-10 leagin-tight text-white">
+            A Design Agency Delivering Success by Winning Hearts
+          </h2>
+
+          <ul className="flex xl:flex-nowrap flex-wrap gap-5 lg:gap-2">
+            <li className="w-full">
+              <p className="text-md sm:text-xl mt-2 text-gray-300">
+                From the moment our company was founded we have helped our clients find <b>exceptional solutions for their bussinesses</b> memorable brnds and digital
+                products. Our expertise grows with each year, and our accumulated experience.
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="w-full lg:w-[40%] flex justify-center items-center mt-12">
+          <div className="flex items-center justify-center w-[250px] h-[250px] relative border">
+            <img
+              src={aboutLogo}
+              alt="aboutLogo"
+              className="invert brightness-0"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 py-[8%] xl:py-[4%] sm:grid-cols-3 gap-12 px-[2%] md:px-[8%] xl:px-[12%]">
+        <div className="text-center">
+          <h2 className="text-7xl sm:text-8xl font-bold leading-none"
+            style={{
+              color: "transparent",
+              WebkitTextStroke: "2px white"
+            }}
+          >
+            <CountUp start={0} end={23} duration={2} />
+          </h2>
+
+          <p className="text-gray-300 text-lg font-semibold mt-3">
+            Team members
+          </p>
+        </div>
+
+        <div className="text-center">
+          <h2 className="text-7xl sm:text-8xl font-bold leading-none"
+            style={{
+              color: "transparent",
+              WebkitTextStroke: "2px white"
+            }}
+          >
+            <CountUp start={0} end={99} duration={2.5} />+
+          </h2>
+
+          <p className="text-gray-300 text-lg font-semibold mt-3">
+            Completed projects
+          </p>
+        </div>
+
+        <div className="text-center">
+          <h2 className="text-7xl sm:text-8xl font-bold leading-none"
+            style={{
+              color: "transparent",
+              WebkitTextStroke: "2px white"
+            }}
+          >
+            <CountUp start={0} end={12} duration={2.8} />M
+          </h2>
+
+          <p className="text-gray-300 text-lg font-semibold mt-3">
+            Lines of code
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
 
