b4bc6845197da7f1936f4a3b48e9780ee40ffd12
Jorge Matos <jisapjisap@gmail.com>
2026-06-17 14:29:43 +0200

    feat: add Pricingplan page component with subscription tiers and features list

--- src/Pages/Pricingplan.jsx
+++ src/Pages/Pricingplan.jsx
@@ -1,5 +1,6 @@
 import { Icon } from "@iconify/react"
 import SectionBanner from "../Components/SectionBanner"
+import { Link } from "react-router-dom"
 
 
 const plans = [
@@ -60,7 +61,7 @@ const Pricingplan = () => {
         {plans.map((plan, idx) => (
           <div key={idx} className={`border border-gray-50/20 rounded-lg relative ${plan.recommended ? "shadow-[0px_0px_25px_rgba(0,0,0,0.2)]" : ""}`}>
             {plan.recommended && (
-              <span className="absolute -top-4 right-6 bg-white text-black text-sm py-1 rounded-sm">
+              <span className="absolute -top-4 right-6 bg-white text-black text-sm py-1  px-2 rounded-sm ">
                 Recommended
               </span>
             )}
@@ -69,6 +70,25 @@ const Pricingplan = () => {
               <h3 className="text-2xl font-semibold">{plan.title}</h3>
               <p className="text-gray-500 text-sm mt-2">{plan.duration}</p>
               <h2 className="text-4xl font-bold mt-4">{plan.price}</h2>
+            </div>
+
+            <div className="w-14 h-14 bg-white text-black rounded-full flex items-center justify-center absolute top-42 left-10">
+              <Icon icon={plan.Icon} className="text-3xl" />
+            </div>
+
+            <ul className="px-8 mt-10 mb-8 space-y-4 text-white">
+              {plan.features.map((item, i) => (
+                <li key={i} className={`flex items-center gap-3 ${!item.active ? "line-through text-gray-400" : ""}`}>
+                  <Icon icon="mdi:check-bold" className="text-green-500" />
+                  {item.text}
+                </li>
+              ))}
+            </ul>
+
+            <div className="p-6 border-t border-gray-50/20 flex justify-center">
+              <Link to="/about" className="btn rounded-full w-[250px]">
+                <span>Purchase Now</span>
+              </Link>
             </div>
           </div>
         ))}
