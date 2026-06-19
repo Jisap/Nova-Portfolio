cd7e72823374858c783ccddaae5913fdb9c080db
Jorge Matos <jisapjisap@gmail.com>
2026-06-19 15:06:18 +0200

    feat: implement projects gallery page with category filtering functionality

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import SectionBanner from "../Components/SectionBanner"
 import project1 from "../assets/project-01.jpg"
 import project2 from "../assets/project-02.jpg"
@@ -7,6 +8,7 @@ import project5 from "../assets/project-05.jpg"
 import project6 from "../assets/project-06.jpg"
 import project7 from "../assets/project-07.jpg"
 import project8 from "../assets/project-08.jpg"
+import { Link } from "react-router-dom"
 
 const projects = [
   {
@@ -14,24 +16,28 @@ const projects = [
     image: project1,
     category: "Branding",
     title: "Museums Art Concept",
+    category: "Branding",
   },
   {
     id: 2,
     image: project2,
     category: "Marketing",
     title: "Market Economy Graphics",
+    category: "Marketing",
   },
   {
     id: 3,
     image: project3,
     category: "Design",
     title: "Headphones 3D Rendering",
+    category: "Design",
   },
   {
     id: 4,
     image: project4,
     category: "Branding",
     title: "Product Packaging Style",
+    category: "Branding",
   },
   {
     id: 5,
@@ -63,7 +69,23 @@ const projects = [
   }
 ];
 
+const categories = [
+  "All Projects",
+  "Branding",
+  "Marketing",
+  "Design",
+  "Architecture",
+  "Photography",
+];
+
 const Projects = () => {
+
+  const [filter, setFilter] = useState("All Projects");
+  const filteredData = filter === "All Projects"
+    ? projects
+    : projects.filter(item => item.category === filter);
+
+
   return (
     <>
       <SectionBanner
@@ -74,7 +96,48 @@ const Projects = () => {
           { label: "Our Proejcts", path: "/projects" }
         ]}
       />
+
+      <div className="py-[8%] px-[5%] xl:px-[12%] flex flex-col lg:flex-row">
+        <div className="lg:w-[25%] p-5 rounded-lg">
+          <ul className="text-gray-400 space-y-5 text-xl">
+            {categories.map((category, index) => (
+              <li key={index}>
+                <button
+                  onClick={() => setFilter(category)}
+                  className={`cursor-pointer hover:text-white transition-all duration-300
+                    ${filter === category ? "text-white font-semibold" : ""}
+                  `}
+                >
+                  {category}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="lg:w-[75%] grid grid-cols-1 md:grid-cols-2 gap-14">
+          {filteredData.map((project) => (
+            <div key={project.id} className="border-b border-gray-300/20 p-5 rounded-lg group">
+              <div className="h-[350px] w-full rounde-lg overflow-hidden mb-5">
+                <img src={project.image} alt="" className="w-full h-full group-hover:scale-110 object-cover transition-all duration-300" />
+              </div>
+
+              <Link to={`/projects/${project.id}`}>
+                <span className="text-white border border-gray-50/20 px-2 py-1 font-semibold text-lg rounded-sm">
+                  {project.category}
+                </span>
+
+                <h3 className="text-2xl text-white sm:text-3xl font-semibold mt-2 hover:text-primary duration-300 transition-all">
+                  {project.title}
+                </h3>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
+
+
   )
 }
 
