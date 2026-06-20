ec4aebc28a9949ebc7c9f52458eaec36a51b1be3
Jorge Matos <jisapjisap@gmail.com>
2026-06-20 12:48:01 +0200

    feat: implement Blog page with listing component and initialize test canvas

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import SectionBanner from "../Components/SectionBanner"
 import blog1 from "../assets/blog_01.jpg"
 import blog2 from "../assets/blog_02.jpg"
@@ -69,6 +70,51 @@ const Blog = () => {
           { label: "Our Blog", path: "/blog" }
         ]}
       />
+
+      {/* Blog */}
+      <div className="py-[8%] px-[2%] md:px-[8%] xl:px-[12%] grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10">
+        {blogs.map((blog) => (
+          <div
+            key={blog.id}
+            className="blog-item group flex flex-col text-white"
+          >
+            <div className="blog-item group flex flex-col text-white">
+              <img
+                src={blog.image}
+                alt={blog.title}
+                className="group-hover:scale-110 transition-all duration-300"
+
+              />
+            </div>
+
+            <div className="blog-content pt-5">
+              <p className="text-lg font-semibold pb-2">
+                <span className="bg-lime-300 text-black px-3 py-1 rounded-full me-3">
+                  {blog.date}
+                </span>
+
+                {blog.category}
+              </p>
+
+              <Link
+                to={`/blog/${blog.id}`}
+                className="text-xl font-semibold hover:text-lime-300 transition-all duration-300"
+              >
+                {blog.title}
+              </Link>
+
+              <p className="text-md text-gray-300 mt-3">
+                {blog.description}
+              </p>
+            </div>
+          </div>
+
+        ))}
+
+
+
+
+      </div>
     </>
   )
 }
