649ab0fbcd413f41a52e72a3b374572fd3adaa25
Jorge Matos <jisapjisap@gmail.com>
2026-06-20 15:23:19 +0200

    feat: add contact page with inquiry form and accessible FAQ section

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -92,7 +92,7 @@ const Contact = () => {
             width="100%"
             height="100%"
             loading="lazy"
-            allowfullscreen
+            allowFullScreen
             title="Google Maps"
             style={{
               border: 0,
